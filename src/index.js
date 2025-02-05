4f065f4f1cb9d298fa8cc11ae46e26108c243df6
Gourav Ahlawat <cse210001019@gmail.com>
2025-02-05 21:54:21 +0530

    patient CRUD operations

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ import 'dotenv/config';
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import Doctor from "./models/Doctor.js";
 import doctorRouter from "./routes/doctorRouter.js";
-
+import patientRouter from "./routes/patientRouter.js";
 const app = express();
 connectDB();
 
@@ -15,4 +14,5 @@ app.get("/", (req, res) => res.send("API is running"));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 app.use('/doctor', doctorRouter)
+app.use('/patient', patientRouter)
 
